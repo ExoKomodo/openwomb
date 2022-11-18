adbd9f0de2a58e22d2f145945bf816229029a6ae
James Orson <jamesaorson@gmail.com>
2022-11-18 11:20:58 -0800

    WOMB-13: Add input namespace with Mouse module

--- src/Womb/Engine/Internals.fs
+++ src/Womb/Engine/Internals.fs
@@ -1,119 +1,44 @@
 module Womb.Engine.Internals
 
+open SDL2Bindings
 open Womb.Graphics
 open Womb.Logging
+open Womb.Types
 
-///////////
-// Types //
-///////////
+let internal handleQuit (config:Config<'T>) (event:SDL.SDL_Event) =
+  config.StopHandler config
 
-type Config =
-  { DisplayConfig: Display.Config;
-    IsRunning: bool;
-    IsShutdown: bool;
-    ExitCode: int;
-    ResourcePathRoot: string;
-    StopHandler: Config -> Config }
-
-    static member Default =
-      { DisplayConfig = Display.Config.Default
-        IsRunning = false
-        IsShutdown = false
-        ExitCode = 0
-        ResourcePathRoot = "Resources"
-        StopHandler = fun config ->
-          debug "Handling quit event and may call twice"
-          if config.IsRunning then
-            { config with
-                DisplayConfig = Display.shutdown config.DisplayConfig
-                IsRunning = false }
-          else
-            config }
-
-/////////////
-// Helpers //
-/////////////
-
-let private stop = Config.Default.StopHandler
-
-//////////////
-// Handlers //
-//////////////
-
-open SDL2Bindings
-
-let internal handleQuit (configState:Config * 'T) (event:SDL.SDL_Event) =
-  let (config, state) = configState
-  (config.StopHandler config, state)
-
-let internal handleKeyUp (configState:Config * 'T) (event:SDL.SDL_Event) =
-  let (config, state) = configState
-  match event.key.keysym.sym with
-  | SDL.SDL_Keycode.SDLK_ESCAPE -> (stop config, state)
-  | SDL.SDL_Keycode.SDLK_f -> (
-      { config with
-          DisplayConfig = Display.toggleFullscreen config.DisplayConfig },
-      state
-    )
-  | _ -> configState
-
-let internal handleEvent (configState:Config * 'T) (event:SDL.SDL_Event) =
-  let (config, state) = configState
+let internal handleEvent (config:Config<'T>) (event:SDL.SDL_Event) =
   match event.typeFSharp with
-  | SDL.SDL_EventType.SDL_KEYUP -> handleKeyUp (config, state) event
-  | SDL.SDL_EventType.SDL_QUIT -> handleQuit (config, state) event
-  | _ -> configState
+  | SDL.SDL_EventType.SDL_KEYUP -> config.KeyUpHandler config event
+  | SDL.SDL_EventType.SDL_QUIT -> handleQuit config event
+  | _ -> config
 
 ////////////
 // Module //
 ////////////
 
-let private drawLoop<'T> (drawHandler:Display.Config * 'T -> Display.Config * 'T) (configState:Config * 'T) =
-  let (config, state) = configState
-  let (updatedConfig, updatedState) = drawHandler (config.DisplayConfig, state)
-  (
-    { config with
-        DisplayConfig = updatedConfig },
-    updatedState
-  )
 
-let rec private eventLoop (configState:Config * 'T) =
+let rec private eventLoop (config:Config<'T>) =
   let mutable event = SDL.SDL_Event()
   if SDL.SDL_PollEvent(&event) = 0 then
-    configState
+    config
   else
-    handleEvent configState event |> eventLoop
+    handleEvent config event |> eventLoop
 
-let internal shutdown (configState: Config * 'T) : Config * 'T =
-  let (config, state) = configState
+let internal shutdown (config: Config<'T>) : Config<'T> =
   warn "Shutting down internals"
   SDL.SDL_Quit()
-  (stop config, state)
+  config.StopHandler config
 
-let drawBegin (config:Display.Config) =
+let drawBegin (config:DisplayConfig) =
   Display.clear config
 
-let drawEnd (config:Display.Config) =
+let drawEnd (config:DisplayConfig) =
   Display.swap config
 
-let internal drawHandlerDefault<'T> (configState:Display.Config * 'T) =
-  let (config, state) = configState
-  ( Display.clear config
-      |> Display.swap,
-    state
-  )
-
-let internal initHandlerDefault<'T> (configState:Config * 'T) : Config * 'T =
-  configState
-
-let internal updateDefault<'T>
-  (drawHandler:Display.Config * 'T -> Display.Config * 'T)
-  (configState:Config * 'T) : Config * 'T =
-    eventLoop configState |> drawLoop drawHandler
-
-let rec internal updateLoop<'T> (updateImpl:Config * 'T -> Config * 'T) (configState:Config * 'T) : Config * 'T =
-  let (config, _) = configState
+let rec internal updateLoop<'T> (config:Config<'T>) : Config<'T> =
   if not config.IsRunning then
-    configState
+    config
   else
-    updateImpl configState |> updateLoop updateImpl
+    eventLoop config |> config.DrawHandler |> updateLoop
---
+++ src/Womb/Graphics/Types.fs
@@ -0,0 +1,30 @@
+[<AutoOpen>]
+module Womb.Graphics.Types
+
+open Womb.Backends.OpenGL.Api
+open Womb.Backends.OpenGL.Api.Constants
+open Womb.Logging
+
+///////////
+// Types //
+///////////
+
+open SDL2Bindings
+
+type DisplayConfig =
+  { Width: uint;
+    Height: uint;
+    Title: string;
+    IsFullscreen: bool;
+    WindowFlags: SDL.SDL_WindowFlags;
+    Window: nativeint;
+    Context: nativeint; }
+
+    static member Default =
+      { Width = 800u
+        Height = 600u
+        Title = "Womb"
+        IsFullscreen = false
+        WindowFlags = SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN
+        Window = 0n
+        Context = 0n }
