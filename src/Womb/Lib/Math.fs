afe056a5776983c2e7747d6f2f989eb81d21a55e
James Orson <jamesaorson@gmail.com>
2022-12-15 11:15:14 -0800

    Generalize map

--- src/Womb/Lib/Math.fs
+++ src/Womb/Lib/Math.fs
@@ -2,5 +2,5 @@ module Womb.Lib.Math
 
 open Womb.Lib.Types
 
-let map (a1: single) (a2: single) (b1: single) (b2: single) (s: single): single =
+let inline map a1 a2 b1 b2 s =
     b1 + (s - a1) * (b2 - b1) / (a2 - a1)
